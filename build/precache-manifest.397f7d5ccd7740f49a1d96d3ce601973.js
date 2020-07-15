6513390f30115ec0b2556afc1012840c6cdaa9bb
Ivan Shulga <shg.ivan97@gmail.com>
2020-07-15 19:02:47 +0300

    gh-pages

---
+++ build/precache-manifest.397f7d5ccd7740f49a1d96d3ce601973.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57e2ffec77d1cfb037327110cce05a4",
+    "url": "/react-weight-tracking/index.html"
+  },
+  {
+    "revision": "f21ae9066b27818fa5da",
+    "url": "/react-weight-tracking/static/css/2.09cd1ea1.chunk.css"
+  },
+  {
+    "revision": "ab1954e6b453a55b9f35",
+    "url": "/react-weight-tracking/static/css/main.796a6d8e.chunk.css"
+  },
+  {
+    "revision": "f21ae9066b27818fa5da",
+    "url": "/react-weight-tracking/static/js/2.2e5a40c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-weight-tracking/static/js/2.2e5a40c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1954e6b453a55b9f35",
+    "url": "/react-weight-tracking/static/js/main.b2133f35.chunk.js"
+  },
+  {
+    "revision": "f50606435cd8749304a2",
+    "url": "/react-weight-tracking/static/js/runtime-main.8e34fe08.js"
+  },
+  {
+    "revision": "039ab089ab67ffae8de4027e4c4484c8",
+    "url": "/react-weight-tracking/static/media/change.039ab089.svg"
+  },
+  {
+    "revision": "709a107175600aa23cd745fc450f1ac9",
+    "url": "/react-weight-tracking/static/media/delete-icon.709a1071.svg"
+  }
+]);
